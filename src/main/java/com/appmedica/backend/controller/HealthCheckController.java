d57b730ea06dddfde198ef57d8c6582a0eb04470
Félix <felixdomerizainc@gmail.com>
2025-05-26 00:16:21 +0200

    Arreglo de error

--- src/main/java/com/appmedica/backend/controller/HealthCheckController.java
+++ src/main/java/com/appmedica/backend/controller/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.appmedica.backend.controller;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,12 @@ public class HealthCheckController {
 
     @GetMapping("/")
     public String home() {
+        System.out.println(">>> HOME ENDPOINT LLAMADO <<<");
         return "Backend activo y funcionando.";
+    }
+    @PostConstruct
+    public void init() {
+        System.out.println(">>> HEALTHCHECK CONTROLLER INICIADO <<<");
     }
 
 }
